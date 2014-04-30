11360a5a26f8b6d65fc38f36d37b794b70511870
Victor Kwak <victorkwak@gmail.com>
2014-04-30 02:53:58 -0700

    Testing push

--- 158[Easy]/src/TornNumber.java
+++ 158[Easy]/src/TornNumber.java
@@ -63,6 +63,6 @@ public class TornNumber {
         for (long i = min; i < max; i++) {
             tornNumber(i, divisor);
         }
-        checkDuplicate(); //testing git
+        checkDuplicate();
     }
 }
