90911c8951238f80bafaf7bd3bbeb07ac54e0a5a
Victor kwak <sirwinsalot@gmail.com>
2014-04-30 02:48:27 -0700

    Comments

--- 158[Easy]/src/TornNumber.java
+++ 158[Easy]/src/TornNumber.java
@@ -5,14 +5,8 @@ import java.util.Set;
 import java.util.StringJoiner;
 
 /**
- * I had the other day in my possession a label bearing the number 3 0 2 5 in large figures.
- * This got accidentally torn in half, so that 3 0 was on one piece and 2 5 on the other. On looking
- * at these pieces I began to make a calculation, when I discovered this little peculiarity. If we
- * add the 3 0 and the 2 5 together and square the sum we get as the result, the complete original number
- * on the label! Thus, 30 added to 25 is 55, and 55 multiplied by 55 is 3025. Curious, is it not?
- * <p>
- * Now, the challenge is to find another number, composed of four figures, all different,
- * which may be divided in the middle and produce the same result.
+ * "Torn" numbers. Add the first half and the second half of a number, square, and you get the original number.
+ * Number must not include repeating digits.
  */
 public class TornNumber {
     private static ArrayList<Long> tornArray = new ArrayList<>();
