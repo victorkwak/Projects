dbac7935471f81abbcb5f53abfe72fc99d5f5e78
Victor Kwak <victorkwak@gmail.com>
2014-11-11 20:45:28 -0800

    Fibonacci comments/ cleanup.

--- Personal/Fibonacci/src/Fibonacci.java
+++ Personal/Fibonacci/src/Fibonacci.java
@@ -7,74 +7,65 @@ import java.text.DecimalFormat;
 
 /**
  * Fibonacci number generator. GUI takes n input and outputs nth Fibonacci number. Uses closed form Binet's formula
- * using a phi approximation.
+ * using an approximation of phi.
  */
-public class Fibonacci implements ActionListener{
+public class Fibonacci implements ActionListener {
 
-    private static String number;
-    private static JTextArea numArea = new JTextArea();
-    private static JTextField numInput = new JTextField(10);
+    private static String output;
+    private static JTextArea outputField = new JTextArea();
+    private static JTextField inputField = new JTextField(10);
 
     Fibonacci() {
+        // Frame
         JFrame frame = new JFrame("Fibonacci");
         frame.setLayout(new FlowLayout());
-        frame.setSize(400, 300);
+        frame.setSize(400, 250);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        //Buttons
         JButton calculate = new JButton("Calculate");
         calculate.addActionListener(this);
         JButton clear = new JButton("Clear");
         clear.addActionListener(this);
 
+        //Label
         JLabel n = new JLabel("n:");
 
-        JPanel results = new JPanel();
-        results.setSize(400, 150);
-        numArea.setLineWrap(true);
-        numArea.setColumns(30);
-        numArea.setRows(10);
-        numArea.setEditable(false);
-	    numArea.setWrapStyleWord(true);
-        JScrollPane sPane = new JScrollPane(numArea);
-        results.add(sPane);
-        frame.add(results);
+        //Configure output box
+        outputField.setLineWrap(true);
+        outputField.setColumns(30);
+        outputField.setRows(10);
+        outputField.setEditable(false);
+        outputField.setWrapStyleWord(true);
+        JScrollPane sPane = new JScrollPane(outputField);
+        frame.add(sPane);
 
+        // "other" contains everything besides the outputField. If buttons are just added to the frame, scaling
+        // becomes kind of weird.
         JPanel other = new JPanel();
-
         other.add(n);
-        other.add(numInput);
+        other.add(inputField);
         other.add(calculate);
         other.add(clear);
-
         frame.add(other);
 
-        JDialog dialog = new JDialog((Frame) null, "Results", false);
-        dialog.setSize(200, 200);
-        dialog.setResizable(true);
-        dialog.setVisible(true);
-
         frame.setVisible(true);
+
+        // Default Button
+        JRootPane jRootPane = frame.getRootPane();
+        jRootPane.setDefaultButton(calculate);
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("Calculate")) {
-            fib(numInput.getText());
-            numArea.setText(number);
+        if (e.getActionCommand().equals("Calculate")) { // Press "Calculate" button
+            fib(inputField.getText());
+            outputField.setText(output);
 
-        }else {
-            numArea.setText("");
-            numInput.setText("");
-        }
-    }
-
-    public void dialogAction(ActionEvent event) {
-        if (event.getActionCommand().equals("Ok")) {
-            //something
-        }
-        else if (event.getActionCommand().equals("Cancel")) {
-            //something
+        } else { // Press "Clear" button
+            outputField.setText("");
+            inputField.setText("");
         }
     }
 
@@ -82,29 +73,30 @@ public class Fibonacci implements ActionListener{
         BigDecimal phi;
 
         if (in.equals("0") || in.equals("1")) {
-            number = in;
-        } else { //Binet's Formula
+            output = in;
+        } else {
+            // tiered phi values
             if (Integer.parseInt(in) <= 200) {
                 phi = new BigDecimal("1.61803398874989484820458683436563811772030917980576");
-            }else if (Integer.parseInt(in) <= 400) {
+            } else if (Integer.parseInt(in) <= 400) {
                 phi = new BigDecimal("1.61803398874989484820458683436563811772030917980576" +
                         "28621354486227052604628189024497072072041893911374");
-            }else if (Integer.parseInt(in) < 600) {
+            } else if (Integer.parseInt(in) < 600) {
                 phi = new BigDecimal("1.61803398874989484820458683436563811772030917980576" +
                         "28621354486227052604628189024497072072041893911374" +
                         "84754088075386891752126633862223536931793180060766");
-            }else if (Integer.parseInt(in) < 900) {
+            } else if (Integer.parseInt(in) < 900) {
                 phi = new BigDecimal("1.61803398874989484820458683436563811772030917980576" +
                         "28621354486227052604628189024497072072041893911374" +
                         "84754088075386891752126633862223536931793180060766" +
                         "72635443338908659593958290563832266131992829026788");
-            }else if (Integer.parseInt(in) < 1100) {
+            } else if (Integer.parseInt(in) < 1100) {
                 phi = new BigDecimal("1.61803398874989484820458683436563811772030917980576" +
                         "28621354486227052604628189024497072072041893911374" +
                         "84754088075386891752126633862223536931793180060766" +
                         "72635443338908659593958290563832266131992829026788" +
                         "06752087668925017116962070322210432162695486262963");
-            }else if (Integer.parseInt(in) < 1500) {
+            } else if (Integer.parseInt(in) < 1500) {
                 phi = new BigDecimal("1.61803398874989484820458683436563811772030917980576" +
                         "28621354486227052604628189024497072072041893911374" +
                         "84754088075386891752126633862223536931793180060766" +
@@ -113,7 +105,7 @@ public class Fibonacci implements ActionListener{
                         "13614438149758701220340805887954454749246185695364" +
                         "86444924104432077134494704956584678850987433944221");
 
-            }else {
+            } else {
                 phi = new BigDecimal("1.61803398874989484820458683436563811772030917980576" +
                         "28621354486227052604628189024497072072041893911374" +
                         "84754088075386891752126633862223536931793180060766" +
@@ -125,57 +117,29 @@ public class Fibonacci implements ActionListener{
                         "34166256249407589069704000281210427621771117778053" +
                         "15317141011704666599146697987317613560067087480710" +
                         "131795236894275219484353056783002287856997829");
-//                    "778347845878228911097625003026961561700250464338243776486102838312683303724292" +
-//                    "675263116533924731671112115881863851331620384005222165791286675294654906811317" +
-//                    "159934323597349498509040947621322298101726107059611645629909816290555208524790" +
-//                    "352406020172799747175342777592778625619432082750513121815628551222480939471234" +
-//                    "145170223735805772786160086883829523045926478780178899219902707769038953219681" +
-//                    "986151437803149974110692608867429622675756052317277752035361393621076738937645" +
-//                    "560606059216589466759551900400555908950229530942312482355212212415444006470340" +
-//                    "565734797663972394949946584578873039623090375033993856210242369025138680414577" +
-//                    "995698122445747178034173126453220416397232134044449487302315417676893752103068" +
-//                    "737880344170093954409627955898678723209512426893557309704509595684401755519881" +
-//                    "921802064052905518934947592600734852282101088194644544222318891319294689622002" +
-//                    "301443770269923007803085261180754519288770502109684249362713592518760777884665" +
-//                    "836150238913493333122310533923213624319263728910670503399282265263556209029798" +
-//                    "642472759772565508615487543574826471814145127000602389016207773224499435308899" +
-//                    "909501680328112194320481964387675863314798571911397815397807476150772211750826" +
-//                    "945863932045652098969855567814106968372884058746103378105444390943683583581381" +
-//                    "131168993855576975484149144534150912954070050194775486163075422641729394680367" +
-//                    "319805861833918328599130396072014455950449779212076124785645916160837059498786");
             }
-            BigDecimal psi = BigDecimal.ONE.subtract(phi);
-            BigDecimal one = BigDecimal.ONE.add(BigDecimal.valueOf(Math.sqrt(5)));
-            BigDecimal two = BigDecimal.ONE.subtract(BigDecimal.valueOf(Math.sqrt(5)));
-            BigDecimal three = BigDecimal.valueOf(2).pow(Integer.parseInt(in)).multiply(BigDecimal.valueOf(Math.sqrt(5)));
-            BigDecimal value = (phi.pow(Integer.parseInt(in)).subtract(psi.pow(Integer.parseInt(in)))).divide(phi.subtract(psi));
+            BigDecimal psi = BigDecimal.ONE.subtract(phi); //Recipricol of phi
+            BigDecimal value = //Binets Formula
+                    (phi.pow(Integer.parseInt(in)).subtract(psi.pow(Integer.parseInt(in)))).divide(phi.subtract(psi));
 
             DecimalFormat decimalFormat = new DecimalFormat();
-            decimalFormat.setMaximumFractionDigits(0);
+            decimalFormat.setMaximumFractionDigits(0); // No decimals
             decimalFormat.setGroupingUsed(false);
-            number = decimalFormat.format(value);
-
+            output = decimalFormat.format(value);
         }
     }
 
-    public static boolean isNumeric(String str) {
-        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
-    }
-
     public static void main(String[] args) {
-
-	    try {
-		    for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-			    if ("Nimbus".equals(info.getName())) {
-				    UIManager.setLookAndFeel(info.getClassName());
-				    break;
-			    }
-		    }
-	    } catch (Exception e) {
-		    // If Nimbus is not available, you can set the GUI to another look and feel.
-	    }
-
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Nimbus not available");
+        }
         SwingUtilities.invokeLater(Fibonacci::new);
-
     }
 }
